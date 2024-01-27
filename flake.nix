966a6838dff0a2f64d57587e31f000b321fd29a3
Benjamin Herzig <ber.bherzig@pm.me>
2024-01-28 00:40:29 +0100

    add raspberry pi flake

---
+++ flake.nix
@@ -0,0 +1,12 @@
+{
+  description = "NixOS configuration with flakes";
+  inputs.nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+
+  outputs = { self, nixpkgs, nixos-hardware }: {
+    nixosConfigurations.pix = nixpkgs.lib.nixosSystem {
+      modules = [
+        nixos-hardware.nixosModules.raspberry-pi-4
+      ];
+    };
+  };
+}
