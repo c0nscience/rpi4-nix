1bf638dd6cc27491922e3ee20a6878d39ef6c2a5
Benjamin Herzig <ber.bherzig@pm.me>
2024-01-28 01:15:27 +0100

    fix compile error

--- configuration.nix
+++ configuration.nix
@@ -9,7 +9,7 @@ in {
     ./hardware-configuration.nix
   ];
 
-  nix.settings.experimental-features = [ "nix-command" "flakes" ]
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   boot = {
     kernelPackages = pkgs.linuxKernel.packages.linux_rpi4;
